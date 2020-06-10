0837d66bd85ad3eaabf3b41a78ee1e46c3f4f06c
Christopher Leigh <christopherleigh23@gmail.com>
2020-06-10 15:55:02 +0200

    Create Power of Two.py

---
+++ Problemsets/Power of Two.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        x = 0
+        while pow(2, x) <= n:
+            if pow(2,x) == n:
+                return True
+            else:
+                x+=1
+        return False
