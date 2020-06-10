401d922b8aac673c491030a75c45998d68c468bc
Christopher Leigh <christopherleigh23@gmail.com>
2020-06-10 10:43:27 +0200

    Create Day3.py

---
+++ JuneDailyChallenges/Week 2/Day3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        position = 0
+        for x in range(len(nums)):
+            if nums[x] == target:
+                return x
+            else:
+                if target > nums[x]:
+                    position = x+1
+        return(position)
+                
